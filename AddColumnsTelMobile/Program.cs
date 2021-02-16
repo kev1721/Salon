08271bd9630057c3d23d844044ec47011653e2ea
T-1000 <t172192@rambler.ru>
2021-02-16 22:46:28 +0400

    Добавлен проект, который обновляет БД: в таблицу Clients добавляет два новых столбца TelMobile2 TelMobile3. Для обеспечения работы программы и БД с тремя мобильными телефонами у клиентов.

---
+++ AddColumnsTelMobile/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using log4net;
+using log4net.Config;
+
+namespace AddColumnsTelMobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("START...");
+            DBUtil dbut = new DBUtil();
+            var res1 = dbut.ConnectDB();
+            var res2 = dbut.AddColumnsTelMobile();
+            var res3 = dbut.DisconnectDB();
+            Console.WriteLine(res1.ToString() + " " + res2.ToString() + " " + res3.ToString());
+            Console.WriteLine("FINISH!!! PRESS ANY KEY");
+            Console.ReadLine();
+        }
+    }
+
+    public class DBUtil
+    {
+        public static readonly ILog log = LogManager.GetLogger(typeof(DBUtil));
+
+        public DBUtil()
+        {
+            string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Application.StartupPath + "\\Database02.mdb";
+            conn = new OleDbConnection(connStr);
+        }
+
+        public OleDbConnection conn;
+
+        public bool ConnectDB()
+        {
+            try
+            {
+                conn.Open();
+                log.Info("OK - connect to database");
+                return true;
+            }
+            catch
+            {
+                log.Error("Error - connect to database");
+            }
+            return false;
+        }
+
+        public bool DisconnectDB()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+                log.Info("OK - disconnect from database");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error - disconnect from database. " + ex.ToString());
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Add columns TelMobile2, TelMobile3
+        /// </summary>
+        /// <param name="idVisit"></param>
+        /// <param name="_matVisit"></param>
+        /// <returns></returns>
+        public bool AddColumnsTelMobile()
+        {
+            bool flag = true;
+
+            if (conn.State != System.Data.ConnectionState.Open)
+                try
+                {
+                    conn.Open();
+                }
+                catch
+                {
+                    return false;
+                }
+
+                string addColumnsSQL = "ALTER TABLE [Clients] ADD COLUMN TelMobile2 TEXT(255), TelMobile3 TEXT(255) ";
+
+                using (OleDbCommand command = new OleDbCommand(addColumnsSQL))
+                {
+                    command.Connection = conn;
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        log.Info("SQL. OK - Adding columns TelMobile2 and TelMobile into Clients table");
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error("SQL. Error - Adding columns TelMobile2 and TelMobile into Clients table");
+
+                        flag = false;
+                    }
+                }
+            
+            return flag;
+        }
+        
+    }
+}
